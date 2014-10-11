9b16091da705354a003c2a9231ef0839cbe82e06
overaction00 <overaction00@gmail.com>
2014-10-11 20:58:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plan::Application.config.secret_token = '06f9691fa4981e961b77d5a6d9728384536b58cc3e8a0b9c24e5e894c8f78b2ec50a42e2a15e3a18e3aa1f9dcb467b29e7db01ca792b1ce429b5b4d68ca95b9b'
