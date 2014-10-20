21fe7ff14d9c0ac3d82679f94c4037bde253aa4e
overaction00 <overaction00@gmail.com>
2014-10-21 00:44:48 +0900

    improve: model validation 추가

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,7 @@ class Page < ActiveRecord::Base
   attr_accessible :name, :desc, :created_at, :updated_at
 
   validates_presence_of :name
+  validates_uniqueness_of :name
 
   has_many :page_items
   has_many :items, through: :page_items
